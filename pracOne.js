30301b96ede8dda3ab34d109ea25379f9d9a25dd
Saccharin01 <chousik01@gmail.com>
2024-07-01 14:34:33 +0900

    nice Try
    컴파일은 완료되었음. 인터페이스에 대한 이해가 필요

---
+++ pracOne.js
@@ -0,0 +1,8 @@
+var items = [
+    { name: "콜라", stock: 5 },
+    { name: "사이다", stock: 0 },
+    { name: "물", stock: 10 },
+    { name: "쥬스", stock: 0 },
+];
+var availableItem = items.filter(function (elemenet) { return elemenet.stock > 0; });
+console.log(availableItem);
