a0c2c100a767ef74672a3be1cc84fcf075770abb
Saccharin01 <chousik01@gmail.com>
2024-07-01 23:47:00 +0900

    feat | pracTwo.ts

--- pracTwo.ts
+++ pracTwo.ts
@@ -30,3 +30,36 @@ class square {
 
 let fourByFour = new square(4,4)
 console.log(fourByFour)
+fourByFour.report()
+console.log(fourByFour.calculateSize())
+
+class triangle {
+  constructor(public angleOne : number, public angleTwo : number, public angleThree : number){
+    this.angleOne = angleOne
+    this.angleTwo = angleTwo
+    this.angleThree = angleThree
+  }
+  isTriange() : string {
+    if(this.angleOne + this.angleTwo + this.angleThree === 180){
+      return  "this is triangle :)"
+    } else {
+      return "this is not triangle :("
+    } 
+  }
+
+    // if(this.angleOne + this.angleTwo + this.angleThree > 180 || this.angleOne + this.angleTwo + this.angleThree <180){
+    //   return `it is not Tringle :(`
+    // } else {
+    //   return `it is Triangle! :)`
+    // }
+  // }
+}
+
+
+let yesTriangle = new triangle(60,40,80)
+let noTriangle = new triangle(90,60,60)
+
+console.log(yesTriangle)
+console.log(noTriangle)
+console.log(yesTriangle.isTriange())
+console.log(noTriangle.isTriange())
